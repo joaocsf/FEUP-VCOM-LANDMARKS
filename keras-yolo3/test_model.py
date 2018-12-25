733c6d83fba433d1e8e8fab6c6c69831f3e28a3b
Hélder Antunes <helder_antunes-@hotmail.com>
2018-12-25 20:54:09 +0000

    test model script.

---
+++ keras-yolo3/test_model.py
@@ -0,0 +1,23 @@
+import os
+from yolo import YOLO
+from PIL import Image
+
+def main():
+  resultsFile = open('results.txt', 'w')
+  with open('test.txt') as f:
+    lines = f.readlines()
+  
+  yolo = YOLO()  
+
+  for i in range(len(lines)):
+    row = lines[i]
+    rowSplited = row.split(' ')
+    imgPath = rowSplited[0]
+    print(i, '/', len(lines))
+    image = Image.open(imgPath)
+    r_image = yolo.vcom_detect_image(image, imgPath, resultsFile)
+
+  yolo.close_session()
+
+if __name__ == '__main__':
+    main()
